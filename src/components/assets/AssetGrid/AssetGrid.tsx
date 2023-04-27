39d952b6b5e5a148edae7d6ab407180adc28938d
Jycssu <contact@jycssu.com>
2023-04-27 12:41:36 +0200

    fix: ensures that asset cards have the same height

--- src/components/assets/AssetGrid/AssetGrid.tsx
+++ src/components/assets/AssetGrid/AssetGrid.tsx
@@ -1,13 +1,11 @@
 import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
-import { useSelector } from 'react-redux'
 
 import { Badge, Button, Card, Grid, Group, Image, Text } from '@mantine/core'
 
 import { Divider } from 'src/components/Divider'
 import {
   Realtoken,
-  selectOwnedRealtokens,
 } from 'src/store/features/wallets/walletsSelector'
 
 const AssetCard: FC<{ value: Realtoken }> = (props) => {
@@ -15,7 +13,7 @@ const AssetCard: FC<{ value: Realtoken }> = (props) => {
   const { t } = useTranslation('common', { keyPrefix: 'assetCard' })
 
   return (
-    <Card shadow='sm' radius='md' withBorder>
+    <Card shadow={"sm"} radius={"md"} withBorder={true} style={{ height: '100%' }}>
       <Card.Section>
         <Image
           src={props.value.imageLink[0]}
@@ -24,71 +22,71 @@ const AssetCard: FC<{ value: Realtoken }> = (props) => {
         />
       </Card.Section>
 
-      <Group position='apart' mt='md'>
+      <Group position={"apart"} mt={"md"}>
         <Text weight={500}>{props.value.shortName}</Text>
-        <Badge variant='light'>
+        <Badge variant={"light"}>
           {tNumbers('currency', { value: props.value.value })}
         </Badge>
       </Group>
 
-      <Divider height={1} my='xs' />
+      <Divider height={1} my={"xs"} />
 
-      <Group position='apart'>
-        <Text size='sm'>{t('tokens')}</Text>
-        <Text size='sm'>
+      <Group position={"apart"}>
+        <Text size={"sm"}>{t('tokens')}</Text>
+        <Text size={"sm"}>
           {tNumbers('decimal', { value: props.value.amount })}
           {' / '}
           {tNumbers('integer', { value: props.value.totalTokens })}
         </Text>
       </Group>
 
-      <Group position='apart'>
-        <Text size='sm'>{t('apr')}</Text>
-        <Text size='sm'>
+      <Group position={"apart"}>
+        <Text size={"sm"}>{t('apr')}</Text>
+        <Text size={"sm"}>
           {tNumbers('percent', { value: props.value.annualPercentageYield })}
         </Text>
       </Group>
 
-      <Group position='apart'>
-        <Text size='sm'>{t('weekly')}</Text>
-        <Text size='sm'>
+      <Group position={"apart"}>
+        <Text size={"sm"}>{t('weekly')}</Text>
+        <Text size={"sm"}>
           {tNumbers('currency', {
             value: props.value.amount * props.value.netRentDayPerToken * 7,
           })}
         </Text>
       </Group>
 
-      <Group position='apart'>
-        <Text size='sm'>{t('yearly')}</Text>
-        <Text size='sm'>
+      <Group position={"apart"}>
+        <Text size={"sm"}>{t('yearly')}</Text>
+        <Text size={"sm"}>
           {tNumbers('currency', {
             value: props.value.amount * props.value.netRentYearPerToken,
           })}
         </Text>
       </Group>
 
-      <Group position='apart'>
-        <Text size='sm'>{t('propertyValue')}</Text>
-        <Text size='sm'>
+      <Group position={"apart"}>
+        <Text size={"sm"}>{t('propertyValue')}</Text>
+        <Text size={"sm"}>
           {tNumbers('currency', {
             value: props.value.totalInvestment,
           })}
         </Text>
       </Group>
 
-      <Divider height={1} my='xs' />
+      <Divider height={1} my={"xs"} />
 
-      <Text size='xs' align='center' mb='xs'>
+      <Text size={"xs"} align={"center"} mb={"xs"}>
         {props.value.fullName}
       </Text>
 
       <Button
-        component='a'
-        fullWidth
-        variant='outline'
-        size='xs'
+        component={"a"}
+        fullWidth={true}
+        variant={"outline"}
+        size={"xs"}
         href={props.value.marketplaceLink}
-        target='_blank'
+        target={"_blank"}
       >
         {t('viewOnRealt')}
       </Button>
