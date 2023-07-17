7f6476b6dbe823d899602eca845fa3353da0d915
Jycssu <contact@jycssu.com>
2023-07-17 18:05:54 +0200

    style: order properties on realtoken api by business logic and add some property types

--- src/types/APIRealToken.ts
+++ src/types/APIRealToken.ts
@@ -10,7 +10,12 @@ export enum APIRealTokenCurrency {
 export enum APIRealTokenPropertyType {
   SingleFamily = 1,
   MultiFamily = 2,
-  Commercial = 3,
+  Duplex = 3,
+  Condominium = 4,
+  MixedUse = 6,
+  Quadplex = 8,
+  Commercial = 9,
+  SFRPortfolio = 10,
 }
 
 export enum APIRealTokenSellPropertyTo {
@@ -35,54 +40,34 @@ interface APIRealTokenDate {
 }
 
 export interface APIRealToken {
+  uuid: string
+  seriesNumber: number
+  tokenIdRules: number
+
+  // General
   fullName: string
   shortName: string
   symbol: string
-  tokenPrice: number
+  marketplaceLink: string
   canal: APIRealTokenCanal
-  currency: APIRealTokenCurrency
+  sellPropertyTo: APIRealTokenSellPropertyTo
   totalTokens: number
   totalTokensRegSummed: number
-  uuid: string
-  ethereumContract: string | null
-  xDaiContract: string | null
-  gnosisContract: string | null
-  goerliContract: string | null
-  totalInvestment: number
-  grossRentYear: number
-  grossRentMonth: number
-  propertyManagement: number
-  propertyManagementPercent: number
-  realtPlatform: number
-  realtPlatformPercent: number
-  insurance: number
-  propertyTaxes: number
-  utilities: number
-  initialMaintenanceReserve: number | null
-  netRentDay: number
-  netRentMonth: number
-  netRentYear: number
-  netRentDayPerToken: number
-  netRentMonthPerToken: number
-  netRentYearPerToken: number
-  annualPercentageYield: number
-  coordinate: {
-    lat: string
-    lng: string
-  }
-  marketplaceLink: string
-  imageLink: string[]
-  propertyType: APIRealTokenPropertyType
-  squareFeet: number | null
-  lotSize: number | null
-  bedroomBath: string | null
-  hasTenants: boolean
-  rentedUnits: number
-  totalUnits: number
-  termOfLease: null
-  renewalDate: null
-  section8paid: number
-  sellPropertyTo: APIRealTokenSellPropertyTo
+  tokenPrice: number
+  currency: APIRealTokenCurrency
+
+  // Dates
+  rentStartDate: APIRealTokenDate
+  lastUpdate: APIRealTokenDate
+  initialLaunchDate: APIRealTokenDate
+
+  // Contracts
+  originSecondaryMarketplaces: {
+    chainId: number
+    chainName: string
+    dexName: string
+    contractPool: string
+  }[]
   secondaryMarketplace: {
     UniswapV1: true | 0
     UniswapV2: string | 0
@@ -123,19 +108,20 @@ export interface APIRealToken {
       chainlinkPriceContract: string | 0
     }
   }
-  underlyingAssetPrice: number
-  renovationReserve: number | null
-  propertyMaintenanceMonthly: number
-  rentStartDate: APIRealTokenDate
-  lastUpdate: APIRealTokenDate
-  originSecondaryMarketplaces: {
-    chainId: number
-    chainName: string
-    dexName: string
-    contractPool: string
-  }[]
-  initialLaunchDate: APIRealTokenDate
-  seriesNumber: number
+  ethereumContract: string | null
+  xDaiContract: string | null
+  gnosisContract: string | null
+  goerliContract: string | null
+
+  // Property details
+  propertyType: APIRealTokenPropertyType
+  coordinate: { lat: string; lng: string }
+  imageLink: string[]
+  squareFeet: number | null
+  lotSize: number | null
+  bedroomBath: string | null
+  termOfLease: null
+  renewalDate: null
   constructionYear: number
   constructionType: string | null
   roofType: string | null
@@ -143,11 +129,41 @@ export interface APIRealToken {
   foundation: string | null
   heating: string | null
   cooling: string | null
-  tokenIdRules: number
+  propertyStories: number | null
+  totalUnits: number
+
+  // Rent related
+  rentedUnits: number
+  rentalType: APIRealTokenRentalType
   rentCalculationType: APIRealTokenRentCalculationType
+  netRentDay: number
+  netRentMonth: number
+  netRentYear: number
+  netRentDayPerToken: number
+  netRentMonthPerToken: number
+  netRentYearPerToken: number
+  annualPercentageYield: number
+  hasTenants: boolean
+  section8paid: number
+  grossRentYear: number
+  grossRentMonth: number
+
+  // Monthly expenses
+  insurance: number
+  propertyTaxes: number
+  propertyManagement: number
+  propertyManagementPercent: number
+  realtPlatform: number
+  realtPlatformPercent: number
+  utilities: number
+  propertyMaintenanceMonthly: number
+
+  // Initial investment
+  underlyingAssetPrice: number
+  renovationReserve: number | null
   realtListingFeePercent: number | null
   realtListingFee: number | null
   miscellaneousCosts: number | null
-  propertyStories: number | null
-  rentalType: APIRealTokenRentalType
+  initialMaintenanceReserve: number | null
+  totalInvestment: number
 }
