caafc3980a45173ba41fc46a2ed2c9e2d533a116
Nandy Bâ <nandyba@yahoo.fr>
2023-09-04 21:21:04 +0200

    fix issue related to ethers version

--- src/store/features/rates/usexDAIUSDRate.ts
+++ src/store/features/rates/usexDAIUSDRate.ts
@@ -8,7 +8,7 @@ const usexDAIUSDRate = (): number | undefined => {
     (async () => {
       try {
         const RPC = 'https://rpc.ankr.com/gnosis';
-        const provider = new ethers.JsonRpcProvider(RPC);
+        const provider = new ethers.providers.JsonRpcProvider(RPC);
         const contract = new ethers.Contract(
           '0x678df3415fc31947dA4324eC63212874be5a82f8',
           ['function latestAnswer() view returns (int256)'],
@@ -16,7 +16,7 @@ const usexDAIUSDRate = (): number | undefined => {
         );
         const decimals = 8;
         const priceFeedResponse = await contract.latestAnswer();
-        const priceFeed = Number(ethers.formatUnits(priceFeedResponse, decimals));
+        const priceFeed = Number(ethers.utils.formatUnits(priceFeedResponse, decimals));
         setXDaiUSDRate(priceFeed);
       } catch (error) {
         console.error('Error fetching xDai price:', error);
