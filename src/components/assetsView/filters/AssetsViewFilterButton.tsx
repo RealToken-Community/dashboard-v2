db018fe3ba8426fdc12cc58bd9d2cdd3d490d254
Jycssu <contact@jycssu.com>
2023-11-22 13:40:43 +0100

    feat(app): add indicator when some filters are actives

--- src/components/assetsView/filters/AssetsViewFilterButton.tsx
+++ src/components/assetsView/filters/AssetsViewFilterButton.tsx
@@ -1,21 +1,32 @@
 import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
 
-import { Button } from '@mantine/core'
+import { Button, Indicator } from '@mantine/core'
 import { useModals } from '@mantine/modals'
+
+import { useAtom } from 'jotai'
+import { isEqual as _isEqual } from 'lodash'
+
+import { assetsViewDefaultFilter, assetsViewFilterAtom } from 'src/states'
 
 export const AssetsViewFilterButton: FC = () => {
   const { t } = useTranslation('common', {
     keyPrefix: 'assetsViewFilterButton',
   })
   const modals = useModals()
+  const [currentFilter] = useAtom(assetsViewFilterAtom)
+  const hasActiveFilter = !_isEqual(currentFilter, assetsViewDefaultFilter)
 
   function openModal() {
     modals.openContextModal('assetsViewFilterModal', { innerProps: {} })
   }
   return (
-    <Button variant={'subtle'} onClick={openModal} compact={true}>
-      {t('filter')}
-    </Button>
+    <div>
+      <Indicator inline={true} color={'red'} disabled={!hasActiveFilter}>
+        <Button variant={'subtle'} onClick={openModal} compact={true}>
+          {t('filter')}
+        </Button>
+      </Indicator>
+    </div>
   )
 }
