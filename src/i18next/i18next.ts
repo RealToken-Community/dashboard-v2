380016d9be63462692bba99c60324aa86fd7e732
Jycssu <contact@jycssu.com>
2024-01-17 12:55:14 +0100

    style: prettier

--- src/i18next/i18next.ts
+++ src/i18next/i18next.ts
@@ -2,10 +2,10 @@ import { initReactI18next } from 'react-i18next'
 
 import i18next from 'i18next'
 
-import { resources } from './locales'
-
 import 'dayjs/locale/en'
 import 'dayjs/locale/fr'
+
+import { resources } from './locales'
 
 export const DEFAULT_NS = 'common'
 export const FALLBACK_LNG = 'en'
