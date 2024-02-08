86394477c608aa9e710fc0e41c96f33d7479c818
Jycssu <contact@jycssu.com>
2024-02-08 21:48:48 +0100

    fix: typing error on modals

--- src/components/modals/index.ts
+++ src/components/modals/index.ts
@@ -2,9 +2,11 @@ import { WalletModal } from '@realtoken/realt-commons'
 
 import { AssetsViewFilterModal } from '../assetsView/filters/AssetsViewFilterModal'
 import { ManageWalletModal } from './ManageWalletModal'
+import { FC } from 'react'
+import { ContextModalProps } from '@mantine/modals'
 
 export const modals = {
-  web3Wallets: WalletModal,
+  web3Wallets: WalletModal as FC<ContextModalProps<Record<string, unknown>>>,
   assetsViewFilterModal: AssetsViewFilterModal,
   manageWalletModal: ManageWalletModal,
 }
