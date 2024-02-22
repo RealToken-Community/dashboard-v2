dde1710408e8d183fc3f552f7327cbe23b2e8e13
Jycssu <contact@jycssu.com>
2024-02-22 21:09:32 +0100

    fix: round currency values too close to 0

--- src/hooks/useCurrencyValue.ts
+++ src/hooks/useCurrencyValue.ts
@@ -7,5 +7,15 @@ export const useCurrencyValue = (value: number | undefined, fallback = '-') => {
   const { t } = useTranslation('common', { keyPrefix: 'numbers' })
   const { rate, symbol } = useSelector(selectUserCurrency)
 
-  return value ? t('currency', { value: value / rate, symbol }) : fallback
+  if (!value) return fallback
+
+  const rateValue = value / rate
+
+  if (value > 0 && rateValue < 0.01) {
+    return `< ${t('currency', { value: 0.01, symbol })}`
+  } else if (value < 0 && rateValue > -0.01) {
+    return `> ${t('currency', { value: -0.01, symbol })}`
+  }
+
+  return t('currency', { value: rateValue, symbol })
 }
