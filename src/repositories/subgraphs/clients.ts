973b42ffd931421234233747d280516309a5ce70
Jycssu <contact@jycssu.com>
2024-06-08 14:12:16 +0200

    feat: migrate to the decentralized subgraphs

--- src/repositories/subgraphs/clients.ts
+++ src/repositories/subgraphs/clients.ts
@@ -1,36 +1,39 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
+const API_KEY = process.env.NEXT_PUBLIC_THEGRAPH_API_KEY
+
 export const GnosisClient = new ApolloClient({
-  uri: 'https://api.thegraph.com/subgraphs/name/realtoken-thegraph/realtoken-xdai',
+  uri: `https://gateway-arbitrum.network.thegraph.com/api/${API_KEY}/subgraphs/id/FPPoFB7S2dcCNrRyjM5QbaMwKqRZPdbTg8ysBrwXd4SP`,
   cache: new InMemoryCache(),
 })
 
 export const EthereumClient = new ApolloClient({
-  uri: 'https://api.thegraph.com/subgraphs/name/realtoken-thegraph/realtoken-eth',
+  uri: `https://gateway-arbitrum.network.thegraph.com/api/${API_KEY}/subgraphs/id/EVjGN4mMd9h9JfGR7yLC6T2xrJf9syhjQNboFb7GzxVW`,
   cache: new InMemoryCache(),
 })
 
 export const RMM2Client = new ApolloClient({
-  uri: 'https://api.thegraph.com/subgraphs/name/realtoken-thegraph/rmm-realt',
+  uri: `https://gateway-arbitrum.network.thegraph.com/api/${API_KEY}/subgraphs/id/CxvZgcPjmtvFeSoSW9N563K7ZcHCwLVv8kXMbmuBKhv1`,
   cache: new InMemoryCache(),
 })
 
 export const RMM3Client = new ApolloClient({
-  uri: 'https://api.thegraph.com/subgraphs/name/realtoken-thegraph/rmm-v3-gnosis',
+  uri: `https://gateway-arbitrum.network.thegraph.com/api/${API_KEY}/subgraphs/id/2xrWGGZ5r8Z7wdNdHxhbRVKcAD2dDgv3F2NcjrZmxifJ`,
   cache: new InMemoryCache(),
 })
 
 export const RMM3WrapperClient = new ApolloClient({
-  uri: 'https://api.thegraph.com/subgraphs/name/realtoken-thegraph/rmm-v3-wrapper-gnosis',
+  uri: `https://gateway-arbitrum.network.thegraph.com/api/${API_KEY}/subgraphs/id/2dMMk7DbQYPX6Gi5siJm6EZ2gDQBF8nJcgKtpiPnPBsK`,
   cache: new InMemoryCache(),
 })
 
 export const LevinSwapClient = new ApolloClient({
-  uri: 'https://api.thegraph.com/subgraphs/name/levinswap/uniswap-v2',
+  uri: `https://gateway-arbitrum.network.thegraph.com/api/${API_KEY}/subgraphs/id/2zkYvVc8xsmytcEjE9pr523qFcbvqRGxhuGGtraozs66`,
   cache: new InMemoryCache(),
 })
 
 export const YamStatisticsClient = new ApolloClient({
   uri: 'https://api.thegraph.com/subgraphs/name/jycssu-com/yam-history-gnosis',
+  // uri: `https://gateway-arbitrum.network.thegraph.com/api/${API_KEY}/subgraphs/id/4eJa4rKCR5f8fq48BKbYBPvf7DWHppGZRvfiVUSFXBGR`,
   cache: new InMemoryCache(),
 })
