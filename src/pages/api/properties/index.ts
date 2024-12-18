e24c8c5f723931c11968e04e4712646ce6a01e4a
BenoistP <75934369+BenoistP@users.noreply.github.com>
2024-12-18 15:05:50 +0100

    fix: API URLs issue #97 (#98)
    * addresses issue #97
    
    Moves api urls to (.)ENV
    set utls to new api.realtoken.community domain (previously api.realt.community)
    
    * Update .env.sample
    
    missing update of sample .env
    
    * Update .env.sample
    
    * NEXT_PUBLIC_ prefix removed for REALTOKENAPI ENV vars

--- src/pages/api/properties/index.ts
+++ src/pages/api/properties/index.ts
@@ -8,8 +8,10 @@ const getRealTokenList = useCache(
     if (!process.env.COMMUNITY_API_KEY) {
       throw new Error('Missing COMMUNITY_API_KEY env variable')
     }
-
-    const response = await fetch('https://api.realt.community/v1/token', {
+    if (!process.env.REALTOKENAPI) {
+      throw new Error('Missing REALTOKENAPI env variable')
+    }
+    const response = await fetch(process.env.REALTOKENAPI, {
       method: 'GET',
       headers: { 'X-AUTH-REALT-TOKEN': process.env.COMMUNITY_API_KEY },
     })
