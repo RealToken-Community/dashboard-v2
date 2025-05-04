e23d166e3aa9db38d8d00c4256b6cce261844dbf
Sigri <sigri44@hotmail.fr>
2025-05-04 22:10:56 +0200

    Fix spellcheck

--- src/components/assetsView/types/assetProduct.type.ts
+++ src/components/assetsView/types/assetProduct.type.ts
@@ -1,6 +1,6 @@
 export enum AssetProductType {
   ALL = 'all',
-  REAL_EASTATE_RENTAL = 'real_estate_rental',
+  REAL_ESTATE_RENTAL = 'real_estate_rental',
   EQUITY_TOKEN = 'equity_token',
   LOAN_INCOME = 'loan_income',
 }
