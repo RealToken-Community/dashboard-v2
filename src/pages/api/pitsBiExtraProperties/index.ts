d55fdbcd5abd8742c1a874b6270c8511bbb87cf1
BenoistP <75934369+BenoistP@users.noreply.github.com>
2025-07-23 23:11:26 +0200

    Clean
    Todo: display response error reason in logs

--- src/pages/api/pitsBiExtraProperties/index.ts
+++ src/pages/api/pitsBiExtraProperties/index.ts
@@ -20,45 +20,21 @@ const getRealTokenListExtraData = useCache(
         console.warn(`extraProperties: Missing PitsBi API ${envVar} env variable`)
       }
     }
-    console.debug('extraProperties: checking PitsBi API availability...')
-    console.debug(`extraProperties: PitsBi API available: ${APIPitsBi_Env_available}`)
-
     if (!APIPitsBi_Env_available) {
-      console.debug('extraProperties: PitsBi API is not available')
-      // Return empty response as fallback
-      console.debug('extraProperties: Returning empty response as fallback...')
+      console.warn('extraProperties: PitsBi API is not available')
+      // Return empty response
       return []
     }
 
     const PITSBiAPI_GET_ALLTOKENS = `${process.env[APIPitsBiEnv.BASE]}${process.env[APIPitsBiEnv.VERSION]}/${process.env[APIPitsBiEnv.GET_ALLTOKENS]}`
-    console.debug(`extraProperties: PitsBi API GET_ALLTOKENS endpoint: ${PITSBiAPI_GET_ALLTOKENS}`)
-
-    // Use PitsBi API as fallback datasource if RealToken API is not available
-    // PitsBi is 100% compatible with RealToken API
-
-    console.debug('extraProperties: Proceeding to fetch PitsBi API...')
-    const startTime = Date.now()
-    // const pitsBiApiResponse = await fetch(PITSBiAPI_GET_ALLTOKENS, {
-    //   method: 'GET',
-    // })
     const pitsBiApiResponse = await fetchWithRetry(PITSBiAPI_GET_ALLTOKENS, { method: 'GET' }, 2, 5_000)
-    const endTime = Date.now()
-    console.debug('extraProperties: PitsBi API response received')
-    console.debug(`extraProperties: PitsBi API response time: ${endTime - startTime}ms`)
-
-    if (!pitsBiApiResponse.ok) {
-      // throw new Error('Failed to fetch PitsBi API : ' + (await pitsBiApiResponse.text()))
-      const pitsBiResponseText = (await pitsBiApiResponse.text())
-      const pitsBiErrorMsg = `extraProperties: Failed to fetch PitsBi API uri: (${PITSBiAPI_GET_ALLTOKENS}) status: (${pitsBiApiResponse.status}) response : ` + pitsBiResponseText
-      console.error(pitsBiErrorMsg)
-      return []
-    }
-
-    // Return PitsBi API response
-    // TODO:reformat as extra data to match RealToken API response structure
-    // TODO:reformat as extra data to match RealToken API response structure
-    // TODO:reformat as extra data to match RealToken API response structure
-    console.debug('extraProperties: Returning PitsBi API response...')
+    // if (!pitsBiApiResponse.ok) {
+    //   // throw new Error('Failed to fetch PitsBi API : ' + (await pitsBiApiResponse.text()))
+    //   const pitsBiResponseText = (await pitsBiApiResponse.text())
+    //   const pitsBiErrorMsg = `extraProperties: Failed to fetch PitsBi API uri: (${PITSBiAPI_GET_ALLTOKENS}) status: (${pitsBiApiResponse.status}) response : ` + pitsBiResponseText
+    //   console.error(pitsBiErrorMsg)
+    //   return []
+    // }
     return pitsBiApiResponse.json()
   },
   { duration: 1000 * 60 * 60 },
--- src/pages/api/properties/index.ts
+++ src/pages/api/properties/index.ts
@@ -18,145 +18,46 @@ const getRealTokenList = useCache(
         throw new Error(`Missing RealToken Community API ${envVar} env variable`)
       }
     }
-    console.debug('Fetching RealToken list from Community API... (2)')
     const REALTOKEN_COMMUNITY_API_GET_ALLTOKENS = `${process.env[APIRealTokenCommunityEnv.API_BASE]}${process.env[APIRealTokenCommunityEnv.VERSION]}/${process.env[APIRealTokenCommunityEnv.GET_ALLTOKENS]}`
-    console.debug(`RealToken API GET_ALLTOKENS endpoint: ${REALTOKEN_COMMUNITY_API_GET_ALLTOKENS} (3)`)
-
-    // const realTokenApiResponse = await fetch(REALTOKEN_COMMUNITY_API_GET_ALLTOKENS, {
-    //   method: 'GET',
-    //   headers: { [APIRealTokenCommunityEnv.AUTH]: process.env[APIRealTokenCommunityEnv.API_KEY] as string },
-    // })
-
     try {
       const realTokenApiResponse = await fetchWithRetry(REALTOKEN_COMMUNITY_API_GET_ALLTOKENS, {
         method: 'GET',
         headers: { [APIRealTokenCommunityEnv.AUTH]: process.env[APIRealTokenCommunityEnv.API_KEY] as string },
       }, 2, 1_000)
-
-      console.debug('RealToken API response received, returning RealToken list ... (3)')
-
       return realTokenApiResponse.json()
-
     } catch (error) {
       console.error(`Failed to fetch RealToken API:`, error)
-      // throw new Error(`Failed to fetch properties from RealToken API: ${error}`)
+    }
+    // Use Pitsbi API as fallback datasource if RealToken API is not available
+    // Pitsbi is 100% compatible with RealToken API
+    let APIPitsbi_Env_available = true
+    for (const envVar of [
+      APIPitsBiEnv.VERSION,
+      APIPitsBiEnv.BASE,
+      APIPitsBiEnv.GET_LASTUPDATE,
+      APIPitsBiEnv.GET_ALLTOKENS,
+    ]) {
+      if (!process.env[envVar]) {
+        APIPitsbi_Env_available = false
+        console.warn(`Missing Pitsbi API ${envVar} env variable`)
+      }
     }
 
-    // console.debug(`RealToken API response received, status: ${realTokenApiResponse.status} (4)`)
+    if (!APIPitsbi_Env_available) {
+      console.debug('Pitsbi API is not available')
+      throw new Error(`Failed to fetch properties from RealToken API ; PitsBI API environment variables are not set, unable to fetch data.`)
+    }
+    const PITSBIAPI_GET_ALLTOKENS = `${process.env[APIPitsBiEnv.BASE]}${process.env[APIPitsBiEnv.VERSION]}/${process.env[APIPitsBiEnv.GET_ALLTOKENS]}`
+    const pitsbiApiResponse = await fetchWithRetry(PITSBIAPI_GET_ALLTOKENS, {
+      method: 'GET',
+    }, 2, 10_000)
 
-    // const realTokenApiResponseText = !realTokenApiResponse.ok ? (await realTokenApiResponse.text()) : ''
-    // const realTokenApiResponseError = !realTokenApiResponse.ok
-
-    // const realTokenApiErrorMsg = realTokenApiResponseError ? `Failed to fetch RealToken API uri: (${REALTOKEN_COMMUNITY_API_GET_ALLTOKENS}) status: (${realTokenApiResponse.status}) response : ` + (await realTokenApiResponse.text()) : ''
-    // if (realTokenApiResponseError) {
-    //   // throw new Error('Failed to fetch properties : ' + (await realTokenApiResponse.text()))
-    //   // const realTokenApiResponseText = (await realTokenApiResponse.text())
-    //   console.error(realTokenApiErrorMsg)
-
-      // Use Pitsbi API as fallback datasource if RealToken API is not available
-      // Pitsbi is 100% compatible with RealToken API
-
-      console.debug('RealToken API: FAILED query, checking Pitsbi API availability...')
-
-      let APIPitsbi_Env_available = true
-      for (const envVar of [
-        APIPitsBiEnv.VERSION,
-        APIPitsBiEnv.BASE,
-        APIPitsBiEnv.GET_LASTUPDATE,
-        APIPitsBiEnv.GET_ALLTOKENS,
-      ]) {
-        if (!process.env[envVar]) {
-          APIPitsbi_Env_available = false
-          console.warn(`Missing Pitsbi API ${envVar} env variable`)
-        }
-      }
-      console.debug(`Pitsbi API available: ${APIPitsbi_Env_available}`)
-
-      if (!APIPitsbi_Env_available) {
-        console.debug('Pitsbi API is not available')
-        // // Return empty response as fallback
-        // console.debug('Returning empty response as fallback...')
-        // return []
-        throw new Error(`Failed to fetch properties from RealToken API ; PitsBI API environment variables are not set, unable to fetch data.`)
-      }
-
-      console.debug('RealToken API response is not OK, but Pitsbi API is available, proceeding to fetch Pitsbi API...')
-
-      const PITSBIAPI_GET_ALLTOKENS = `${process.env[APIPitsBiEnv.BASE]}${process.env[APIPitsBiEnv.VERSION]}/${process.env[APIPitsBiEnv.GET_ALLTOKENS]}`
-      console.debug(`Pitsbi API GET_ALLTOKENS endpoint: ${PITSBIAPI_GET_ALLTOKENS}`)
-
-      // const pitsbiApiResponse = await fetch(PITSBIAPI_GET_ALLTOKENS, {
-      //   method: 'GET',
-      // })
-      const pitsbiApiResponse = await fetchWithRetry(PITSBIAPI_GET_ALLTOKENS, {
-        method: 'GET',
-      }, 2, 10_000)
-      if (!pitsbiApiResponse.ok) {
-        // throw new Error('Failed to fetch Pitsbi API : ' + (await pitsbiApiResponse.text()))
-        const pitsbiResponseText = (await pitsbiApiResponse.text())
-        // const pitsbiErrorMsg = `Failed to fetch Pitsbi API uri: (${PITSBIAPI_GET_ALLTOKENS}) status: (${pitsbiApiResponse.status}) response : ` + pitsbiResponseText
-        // console.error(pitsbiErrorMsg)
-        throw new Error(`Failed to fetch properties from both RealToken and Pitsbi APIs. Pitsbi error: ${pitsbiResponseText}`)
-      }
-
-
-      // TODO: reformat PitsBI response to match "standard" RealToken API response structure
-      // and return it with PitsBI extra data
-
-      // Return Pitsbi API response if RealToken API is not available
-      console.debug('Returning Pitsbi API response (as RealT API) ...')
-      return pitsbiApiResponse.json()
-
-      // Load Pitsbi "in the background"
-
-
-
-      // console.debug(await pitsbiApiResponse.json())
-
-      // const realTokenList = await realTokenApiResponse.json() as APIRealToken[]
-      // const pitsbiList = await pitsbiApiResponse.json()
-      
-      
-      // console.debug('returning Pitsbi API response...')
-      // return pitsbiApiResponse.json()
-      
-      // console.log('Pitsbi API response received, merging with RealToken API response...')
-      // const jsonMerged = mergeObjects([realTokenApiResponse.json(), pitsbiApiResponse.json()], {
-      //   // Merge options can be specified here if needed
-      // })
-      // console.debug('Merged JSON:', jsonMerged)
-      // return jsonMerged
-
-    // } // If RealToken API response is not OK
-
-
-
-
-      // console.debug('Fetching RealToken list from PitsBI API... 2')
-
-
-
+    // if (!pitsbiApiResponse.ok) {
+    //   const pitsbiResponseText = (await pitsbiApiResponse.text())
+    //   throw new Error(`Failed to fetch properties from both RealToken and Pitsbi APIs. Pitsbi error: ${pitsbiResponseText}`)
     // }
-
-
-
-    // if (APIPitsbi_Env_available) {
-
-    // // console.debug('Pitsbi API is not available')
-
-    //   // if (!realTokenApiResponse.ok) {
-    //   //   throw new Error('Failed to fetch properties : ' + (await realTokenApiResponse.text()))
-    //   // }
-
-    // if (!realTokenApiResponse.ok) {
-    //   throw new Error(`${realTokenApiErrorMsg} and Pitsbi API is not available (missing env variables)`)
-    // }
-
-    // // console.debug('RealToken API response received, returning RealToken list...')
-
-
-    // // return realTokenApiResponse
-    // return realTokenApiResponse.json()
+    // Return Pitsbi API response if RealToken API is not available
+    return pitsbiApiResponse.json()
   },
   { duration: 1000 * 60 * 60 },
 )
