73e4fa8c2d249d68fb236a834ea39a693d0dfbec
Sigri44 <sigri44@hotmail.fr>
2026-02-18 21:59:33 +0100

    Fix prettier

--- src/hooks/useREG.ts
+++ src/hooks/useREG.ts
@@ -106,7 +106,7 @@ const getREG = async (
     )
     availableBalance += balance[WalletType.Ethereum].amount
   }
-  
+
   const regVaultAbiGetUserGlobalStateOnly =
     getRegVaultAbiGetUserGlobalStateOnly()
 
